466740dc96e28aa8705b2cd9f464c8b13aeeee8a
JeisonPC <jeida16@yahoo.es>
2022-10-10 17:03:01 -0500

    Agregando cambios para el modal

---
+++ src/TodoContext/index.js
@@ -0,0 +1,88 @@
+import React from 'react' ;
+import { useLocalStorage } from "./useLocalStorage";
+
+const TodoContext = React.createContext();
+
+function TodoProvider(props) {
+
+    const {
+        item: todos,
+        saveItem: saveTodos,
+        loading,
+        error,
+    } = useLocalStorage('TODOS_V1', []);
+
+    const [searchValue, setSearchValue] = React.useState('');
+    const [openModal , setOpenModal] = React.useState(false)
+    const completedItem = todos.filter(todo => !!todo.completed).length;
+    const totalItem = todos.length;
+
+    let searchedTodos = [];
+
+    if (!searchValue.length >= 1) {
+        searchedTodos = todos;
+    } else {
+
+        searchedTodos = todos.filter(todo => {
+
+            const todoText = todo.text.toLowerCase();
+            const searchText = searchValue.toLowerCase();
+
+            return todoText.includes(searchText);
+        });
+    }
+
+
+
+    const completeTodo = (text) => {
+
+        const todoIndex = todos.findIndex(todo => todo.text === text);
+
+        const newTodos = [...todos];
+        newTodos[todoIndex].completed = true;
+        saveTodos(newTodos);
+
+    };
+
+    const addTodo = (text) => {
+
+        const newTodos = [...todos];
+        newTodos.push({
+            completed:false,
+            text,
+        });
+        saveTodos(newTodos);
+
+    };
+
+    const deleteTodo = (text) => {
+
+        const todoIndex = todos.findIndex(todo => todo.text === text);
+
+        const newTodos = [...todos];
+        newTodos.splice(todoIndex, 1);
+        saveTodos(newTodos);
+
+    };
+
+    return (
+        <TodoContext.Provider value={{
+            loading,
+            error,
+            totalItem,
+            completedItem,
+            searchValue,
+            setSearchValue,
+            searchedTodos,
+            completeTodo,
+            deleteTodo,
+            addTodo,
+            openModal , 
+            setOpenModal,
+        }}>
+            {props.children}
+        </TodoContext.Provider>
+    );
+}
+
+export {TodoContext ,TodoProvider}
---
+++ src/TodoForm/index.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { TodoContext } from "../TodoContext";
+import './TodoForm.css';
+
+function TodoForm() {
+
+    const [newTodoValue, setNewTodoValue] = React.useState('');
+
+    const {
+        addTodo,
+        setOpenModal,
+    } = React.useContext(TodoContext);
+
+    const onCancel = () => {
+        setOpenModal(false);
+    };
+    const onChange = (event) => {
+        setNewTodoValue(event.target.value);
+    };
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        addTodo(newTodoValue);
+        setOpenModal(false);
+    };
+
+    return (
+
+        <form onSubmit={onSubmit}>
+            <label>Escribe tu nuevo TODO</label>
+            <textarea
+                value={newTodoValue}
+                onChange={onChange}
+                placeholder="Cortar la cebolla para el almuerzo"
+
+            />
+            <div>
+                <button
+                    type="button"
+                    onClick={onCancel}
+                    className="TodoForm-button TodoForm-button-cancel"
+                >
+                    Cancelar
+                </button>
+                <button
+                    className="TodoForm-button TodoForm-button-add"
+                    type="submit"
+                >
+                    Añadir
+                </button>
+            </div>
+        </form>
+    );
+}
+
+export { TodoForm };
