2734c8f9db5f8512f8d21f53690d5aed87614293
JeisonPC <jeida16@yahoo.es>
2022-10-11 18:46:34 -0500

    Updates

---
+++ precache-manifest.c33185539801550b5f11e799842cbd1e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "732a2b7b31aff2f123f8",
+    "url": "/TODO-appIntro-react/static/css/main.4cd52d0a.chunk.css"
+  },
+  {
+    "revision": "732a2b7b31aff2f123f8",
+    "url": "/TODO-appIntro-react/static/js/main.732a2b7b.chunk.js"
+  },
+  {
+    "revision": "0355fa1f25d55e4d7ffa",
+    "url": "/TODO-appIntro-react/static/js/1.0355fa1f.chunk.js"
+  },
+  {
+    "revision": "638e3aa317436983b923",
+    "url": "/TODO-appIntro-react/static/js/runtime~main.638e3aa3.js"
+  },
+  {
+    "revision": "badcc6dd70ea438773185096b3bd741c",
+    "url": "/TODO-appIntro-react/static/media/error-image.badcc6dd.png"
+  },
+  {
+    "revision": "bec560d75b919ea700f80342acccd137",
+    "url": "/TODO-appIntro-react/static/media/loading-icon1.bec560d7.png"
+  },
+  {
+    "revision": "8792590e528a23938e69ce8b8b4acb21",
+    "url": "/TODO-appIntro-react/static/media/checked2.8792590e.png"
+  },
+  {
+    "revision": "6e0e9826ca06720b6a8ebe0d6abfe91c",
+    "url": "/TODO-appIntro-react/static/media/check2.6e0e9826.png"
+  },
+  {
+    "revision": "d287b8c722435dd67dd08df28d799c2d",
+    "url": "/TODO-appIntro-react/index.html"
+  }
+];
